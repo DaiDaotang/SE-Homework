eff17bf1051c179018af89f9e3e0d4eef095a3b8
夕汐 <32385788+WhiteCoffeeSama@users.noreply.github.com>
2020-05-22 15:21:28 +0800

    收藏夹Mapper

--- SSM/src/main/java/com/know/dao/FavoritesMapper.java
+++ SSM/src/main/java/com/know/dao/FavoritesMapper.java
@@ -1,15 +1,23 @@
 package com.know.dao;
 
+import com.know.pojo.Favorites;
+
+import java.util.List;
 import java.util.Map;
 
 public interface FavoritesMapper {
     // 新建收藏夹
-    int insertFavorites(Map<String, Object> map);
+    int insertFavorites(Favorites favorites);
     // 删除收藏夹
     int deleteFavorites(int favoritesId);
     // 更改收藏夹
-
+    int updateFavorites(Favorites favorites);
     // 查找收藏夹 by Id
-
-    //
+    Favorites queryFavoritesById(int favoritesId);
+    // 查找收藏夹列表 by userId
+    List<Favorites> queryFavoritesList(int userId);
+    // 收藏内容
+    int like(Map<String, Object> map);
+    // 取消收藏
+    int dislike(Map<String, Object> map);
 }
