708b4acccec4425e8b6b7beec508732fd3c15c70
夕汐 <32385788+WhiteCoffeeSama@users.noreply.github.com>
2020-05-27 17:53:42 +0800

    创建搜索模块

---
+++ SSM/src/main/java/com/know/controller/SearchController.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: SearchController
+ * Author:   夕汐
+ * Date:     2020/5/27 17:52
+ * Description: 搜索Controller
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.know.controller;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈搜索Controller〉
+ *
+ * @author 夕汐
+ * @create 2020/5/27
+ * @since 1.0.0
+ */
+public class SearchController {
+
+}
---
+++ SSM/src/main/java/com/know/service/SearchServiceImpl.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: SearchServiceImpl
+ * Author:   夕汐
+ * Date:     2020/5/27 17:51
+ * Description: 搜索模块
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.know.service;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈搜索模块〉
+ *
+ * @author 夕汐
+ * @create 2020/5/27
+ * @since 1.0.0
+ */
+public class SearchServiceImpl {
+
+}
